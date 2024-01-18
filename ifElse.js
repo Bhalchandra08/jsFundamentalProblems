b2127dad48b52ad725ef42762808d5c78eff1ddf
Bhalchandra08 <bhushandhuri86@gmail.com>
2024-01-18 11:23:56 +0530

    positive, negative or zero Number Finding

---
+++ ifElse.js
@@ -0,0 +1,12 @@
+// Check If a number is positive, negative or zero
+const number = 54;
+if(number > 0){
+    console.log("The Number is Positive");
+}else if(number < 0){
+    console.log("The Number is Negative");
+}else{
+    console.log("The Number is Zero or Not a Number");
+}
+console.log("")
+console.log("**********************************************");
+console.log("**********************************************");
